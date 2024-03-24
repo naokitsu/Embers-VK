26fa64cbf2f922865770f485a616beb8f448c6a2
Naokitsu <naokitsu@tutanota.com>
2024-03-24 20:33:23 +0400

    Add platform detection

---
+++ embers/src/platform_detection.h
@@ -0,0 +1,16 @@
+//
+// Created by Naokitsu on 3/24/2024.
+//
+
+#ifndef EMBERS_PLATFORM_DETECTION_H_
+#define EMBERS_PLATFORM_DETECTION_H_
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+#define EMBERS_PLATFORM WINDOWS
+#elif defined(__linux__) || defined(__gnu_linux__)
+#define EMBERS_PLATFORM LINUX
+#else
+#error "Platform not supported!"
+#endif
+
+#endif //EMBERS_PLATFORM_DETECTION_H_
